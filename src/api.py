6259e072fc666ded21dced3495827d75a6580f40
Bahna-Darius <emanuelbahna@gmail.com>
2025-04-29 15:57:00 +0300

    Add API

--- src/api.py
+++ src/api.py
@@ -1,8 +1,11 @@
 import pandas as pd
-from fastapi import APIRouter
-from src.utils import CompanyData
+from aiohttp.abc import HTTPException
+from fastapi import APIRouter, File, UploadFile, BackgroundTasks
+from src.utils import CompanyData, save_predictions_file
 from src.preprocessing import preprocessing_company_data
 from src.model import predict_insurance_labels
+from src.config import settings
+import asyncio
 
 
 router = APIRouter()
@@ -22,3 +25,45 @@ async def predict_company(data: CompanyData):
             "message": "Predict model succesul!"
         },
     }
+
+
+@router.post("/predict_file", status_code=200)
+async def predict_file(
+        background_tasks: BackgroundTasks,
+        file: UploadFile = File(...),
+):
+
+    try:
+        df = pd.read_csv(file.file)
+    except Exception as e:
+        raise HTTPException(400, f"Could not read CSV: {e}")
+
+    df_clean = await preprocessing_company_data(data=df)
+
+    # Run predictions in batches:
+    async def run_and_save():
+        batch_size = settings.model_params.batch_size
+        preds = []
+        # Loop in batches:
+        for i in range(0, len(df_clean), batch_size):
+            batch = df_clean[i : i + batch_size]
+            loop = asyncio.get_running_loop()
+            result = await loop.run_in_executor(
+                None,
+                lambda b=batch: predict_insurance_labels(b)
+            )
+            preds.extend(await result)
+        print(f"\nData predicted is: {preds}\n")
+        df_clean['insurance_label'] = preds
+        save_predictions_file(df_clean)
+
+    background_tasks.add_task(run_and_save)
+
+    return {
+        "success": True,
+        "message": "File received. Predictions will be saved in background.",
+        "output_dir": settings.preprocess.output_dir,
+    }
+
+
+
